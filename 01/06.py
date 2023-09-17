e692e49a39f930ade879a4a2737507024da65d3e
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-09-17 15:25:24 +0300

    06.py done

--- 01/06.py
+++ 01/06.py
@@ -1,5 +1,21 @@
-answer1 = input('любите ли вы котиков?')
-answer2 = input('пельмень?')
-if (answer1 != "да" and answer1!="нет") or (answer2 != "да" and answer2!="нет"):
-    print("Недопустимый ответ")
-print("Вы обладаете пельменем")
+answer1 = input('Вы студент?')
+answer2 = input('Едите ли вы сырки каждый день?')
+
+student = ("да" in answer1)
+cheese = ("да" in answer2)
+notstudent = ("нет" in answer1)
+notcheese = ("нет" in answer2)
+
+if student:
+    if cheese:
+        print("Вы счастливый студент!")
+    elif notcheese:
+        print("Продолжайте учиться, и удача улыбнётся вам!")
+elif notstudent:
+    if cheese:
+        print("Вы счастливый человек!")
+    elif notcheese:
+        print("У вас ещё всё впереди, главное - не останавливаться!")
+else:
+    print('Недопустимый ответ, вводите только "да" или "нет". ')
+
