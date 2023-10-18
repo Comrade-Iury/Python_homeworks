6c7289766bbf3811616a36a507350e83f01d82d2
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-10-18 21:55:38 +0300

    Add files via upload

---
+++ 02/11.py
@@ -0,0 +1,7 @@
+num = float(input())
+if not(bool(num)):
+    print("0")
+elif num > 0:
+    print("+")
+else:
+    print("-")
---
+++ 02/12.py
@@ -0,0 +1,4 @@
+kop = len(input()) * 40
+rub = kop // 100
+kop %= 100
+print(rub, "р.", kop, "коп.")
