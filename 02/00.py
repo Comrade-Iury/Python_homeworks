1c56488bff96355f4229929346af5b236d5a20e9
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-10-18 20:38:23 +0300

    Delete 02/00.py

--- 02/00.py
+++
@@ -1,6 +0,0 @@
-city_1 = input("Введите город куда поехать в июле")
-city_2 = input("Введите город куда поехать в августе")
-if (city_1 == "Тула" or city_2 == "Пенза") and city_1 != city_2 and not(city_1 == "Тула" and city_2 =="Пенза"):
-	print("да")
-else: 
-	print("нет")
