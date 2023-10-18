95b8bb1ee7afed1ea2e3389ecde923f7fe373104
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-10-18 20:37:48 +0300

    Add files via upload

--- 02/01.py
+++ 02/01.py
@@ -1,1 +1,6 @@
-print(1 * 2  * 3 * 4 * 5 * 6 * 7 * 8 * 9)
+city_1 = input("Введите город куда поехать в июле")
+city_2 = input("Введите город куда поехать в августе")
+if (city_1 == "Тула" or city_2 == "Пенза") and city_1 != city_2 and not (city_1 == "Тула" and city_2 == "Пенза"):
+    print("да")
+else:
+    print("нет")
