ebe5b545706f2134f1cf3d671e45349bd3161b3f
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-11-29 21:11:56 +0500

    05.py is not done

---
+++ 19/05.py
@@ -0,0 +1,15 @@
+def ask_password(login: str, password: str, success=1, failure=0):
+    login, password = login.lower(), password.lower()
+    print(login, password)
+
+
+def right_password():
+    pass
+
+
+def wrong_password():
+    pass
+
+
+def main(login, password):
+    pass
