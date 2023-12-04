b955649167a735383c3977ecf6952beacd326577
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-12-04 23:18:28 +0500

    Add files via upload

--- 12/01.py
+++ 12/01.py
@@ -1,1 +1,6 @@
-
+result = ""
+for i in range(int(input())):
+    new_string = input()
+    if "кот" in new_string:
+        result += f"{i + 1} {new_string.find('кот') + 1} \n"
+print(result)
---
+++ 12/02.py
@@ -0,0 +1,5 @@
+words = input().split()
+for i in range(len(words)):
+    words[i] = len(words[i])
+print(max(words))
+
