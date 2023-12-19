064cbb79eec299234040e36ece18f481cf4d77fd
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2023-12-19 11:22:10 +0500

    Create 01.py

---
+++ 22/01.py
@@ -0,0 +1,11 @@
+import random
+def make_bingo():
+    bingo_matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    print(bingo_matrix)
+    for i in range(5):
+        for j in range(5):
+            bingo_matrix[i][j] = random.randint(1, 75)
+    bingo_matrix[2][2] = 0
+    for i in range(len(bingo_matrix)):
+        bingo_matrix[i] = tuple(bingo_matrix[i])
+    return tuple(bingo_matrix)
