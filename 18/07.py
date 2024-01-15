80b3995d2e2c653685d070fd33c2bce646e2c6a5
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2024-01-15 20:29:03 +0300

    Add files via upload

--- 18/07.py
+++ 18/07.py
@@ -1,10 +1,17 @@
-def transpose(matrix):
-    copied_matrix = list()
-    for i in range(len(matrix)):
-        copied_matrix.append([])
-        for j in range(len(matrix)):
-            copied_matrix[i].append(matrix[i][j])
+def solve(coefficients):
+    if len(coefficients) == 1:
+        return {0}
+    for i in range(len(coefficients)):
+        coefficients[i] = int(coefficients[i])
+    if len(coefficients) == 2:
+        return {- coefficients[1] / coefficients[0]}
+    if len(coefficients) == 3:
+        a, b, c = coefficients[::]
+        d = (b ** 2) - 4 * a * c
+        x1 = (-b + d ** 0.5) / (a * 2)
+        x2 = (-b - d ** 0.5) / (a * 2)
+        return {x1, x2}
 
-    for i in range(len(matrix[0])):
-        for j in range(len(matrix)):
-            matrix[j][i] = copied_matrix[i][j]
+
+print(*solve(input("Введите коэффициенты уравнения через пробел: ").split()))
+
