4d2b84d8c9f4f857488d11d37aac8a66ea54c8bf
Comrade-Iury <114354161+Comrade-Iury@users.noreply.github.com>
2024-01-18 15:47:25 +0300

    Update 06.py

--- 19/06.py
+++ 19/06.py
@@ -1,5 +1,8 @@
 def find_farthest_orbit(list_of_orbits):
     areas = list()
-    [areas.append(list_of_orbits[i][0] * list_of_orbits[i][1]) for i in range(len(list_of_orbits))
-     if list_of_orbits[i][0] != list_of_orbits[i][1]]
+    [areas.append(list_of_orbits[i][0] * list_of_orbits[i][1]) for i in range(len(list_of_orbits))]
+    for i in range(len(areas)):
+        if list_of_orbits[i][0] == list_of_orbits[i][1]:
+            areas[i] = 0
+
     return list_of_orbits[areas.index(max(areas))]
